9618bd5ae1ed4922459660fe867532960dc5ae3b
Eduard Urbach <e.urbach@gmail.com>
2018-10-26 12:33:48 +0900

    Added error handling to the interface

--- Character.go
+++ Character.go
@@ -6,6 +6,6 @@ type Character struct {
 }
 
 // GetCharacter fetches character data for a given character ID.
-func GetCharacter(id string) *Character {
-	return nil
+func GetCharacter(id string) (*Character, error) {
+	return nil, nil
 }
--- CharacterID.go
+++ CharacterID.go
@@ -1,6 +1,23 @@
 package ffxiv
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aerogo/http/client"
+)
+
 // GetCharacterID fetches the first found character ID for the given nick and server.
-func GetCharacterID(nick string, server string) string {
-	return ""
+func GetCharacterID(nick string, server string) (string, error) {
+	// Replace spaces with plus signs
+	nick = strings.Replace(nick, " ", "+", -1)
+
+	url := fmt.Sprintf("https://na.finalfantasyxiv.com/lodestone/character/?q=%s&worldname=%s", nick, server)
+	_, err := client.Get(url).End()
+
+	if err != nil {
+		return "", err
+	}
+
+	return "", nil
 }
