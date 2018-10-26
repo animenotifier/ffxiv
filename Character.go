d2ec98b17491cab02f414525181cba6657a4398e
Eduard Urbach <e.urbach@gmail.com>
2018-10-26 12:13:59 +0900

    Character API

---
+++ Character.go
@@ -0,0 +1,11 @@
+package ffxiv
+
+// Character represents a Final Fantasy XIV character.
+type Character struct {
+	Level int
+}
+
+// GetCharacter fetches character data for a given character ID.
+func GetCharacter(id string) *Character {
+	return nil
+}
